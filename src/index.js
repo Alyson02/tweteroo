88380886f5bafde4842821d33778a17cba128da2
Alyson02 <alyson.subrim2002@gmail.com>
2022-11-07 10:04:00 -0300

    paginação & fix/tweets/:username

--- src/index.js
+++ src/index.js
@@ -14,6 +14,110 @@ const tweets = [
     username: "bobesponja",
     tweet: "eu amo o hub",
   },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
 ];
 
 const app = express();
@@ -74,32 +178,61 @@ app.post("/tweets", (req, res) => {
 });
 
 app.get("/tweets", (req, res) => {
-  const paginas = [];
+  let page = req.query.page;
+  const pages = [];
+  const tweetsReverse = tweets.slice().reverse();
 
-  console.log(tweets.length);
-
-  for (let i = tweets.length - 1; last10Tweets.length < 10 && i >= 0; i--) {
-    const tweet = tweets[i];
-    console.log(i);
-    const user = users.find((x) => x.username === tweet.username);
-
-    if (user === undefined) {
-      res.status(404).send("Usuario não encontrado");
-      return;
-    }
-
-    last10Tweets.push({
-      username: user.username,
-      avatar: user.avatar,
-      tweet: tweet.tweet,
-    });
+  const pageParsed = parseInt(page);
+  if (isNaN(pageParsed) || pageParsed < 1) {
+    res.status(400).send({ message: "Informe uma página válida!" });
+    return;
   }
 
-  res.send(last10Tweets);
+  for (let i = 0; i <= tweetsReverse.length; i += 10) {
+    pages.push(tweetsReverse.slice(i, i + 10));
+  }
+
+  const top10tweetsPaginated = [];
+  pages.forEach((p, numeroPagina) => {
+    numeroPagina++;
+    const page = { numeroPagina: numeroPagina, lst: [] };
+
+    p.forEach((t) => {
+      const user = users.find((x) => x.username === t.username);
+
+      if (user === undefined) {
+        res.status(404).send("Usuario não encontrado");
+        return;
+      }
+
+      page.lst.push({
+        username: user.username,
+        avatar: user.avatar,
+        tweet: t.tweet,
+      });
+    });
+
+    top10tweetsPaginated.push(page);
+  });
+
+  res.send(
+    top10tweetsPaginated.find((x) => x.numeroPagina === Number(page))?.lst
+  );
 });
 
 app.get("/tweets/:username", (req, res) => {
   const { username } = req.params;
   const tweetsFiltrados = tweets.filter((t) => t.username === username);
-  res.send(tweetsFiltrados);
+  const tweetsUser = [];
+
+  tweetsFiltrados.forEach((t) => {
+    const user = users.find((x) => x.username === t.username);
+    tweetsUser.push({
+      username: user.username,
+      avatar: user.avatar,
+      tweet: t.tweet,
+    });
+  });
+
+  res.send(tweetsUser);
 });
